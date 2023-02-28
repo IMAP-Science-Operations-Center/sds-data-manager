a191fa39974004f3684d361ac75d5e1de74d9199
Bryan Harter <harter864@gmail.com>
2023-02-28 16:44:54 -0700

    A few more issues from the merge

--- sds_data_manager/lambda_code/SDSCode/upload_api.py
+++ sds_data_manager/lambda_code/SDSCode/upload_api.py
@@ -11,12 +11,10 @@ logger.setLevel(logging.INFO)
 def _load_allowed_filenames():
     """
     Load the config.json file as a python dictionary.
-    
     :return: dictionary object of file types and their attributes.
     """
     current_dir = os.path.dirname(__file__)
     config_file = os.path.join(current_dir, "config.json")
-    
     with open(config_file) as f:
         data = json.load(f)
     return data
@@ -24,17 +22,13 @@ def _load_allowed_filenames():
 def _check_for_matching_filetype(pattern, filename):
     """
     Read a pattern from config.json and compare it to the desired filename.
-
     :param pattern: A file naming pattern from the config.json
     :param filename: String name of the desired file name.
-    
     :return: The file_dictionary, or None if there is no match. 
     """
     split_filename = filename.replace("_", ".").split(".")
-
     if len(split_filename) != len(pattern):
         return None
-    
     i = 0
     file_dictionary = {}
     for field in pattern:
@@ -51,10 +45,8 @@ def _check_for_matching_filetype(pattern, filename):
 def _generate_signed_upload_url(filename, tags={}):
     """
     Create a presigned url for a file in the SDS storage bucket.
-    
     :param filename: Required.  A string representing the name of the object to upload.
     :param tags: Optional.  A dictionary that will be stored in the S3 object metadata.  
-
     :return: A URL string if the file was found, otherwise None.
     """
     filetypes = _load_allowed_filenames()
@@ -64,15 +56,15 @@ def _generate_signed_upload_url(filename, tags={}):
         if metadata is not None:
             break
     if metadata is None:
-        logger.info(f"Found no matching file types to index this file against.")
+        logger.info("Found no matching file types to index this file against.")
         return None
     bucket_name = os.environ["S3_BUCKET"]
-    url = boto3.client('s3').generate_presigned_url(
-        ClientMethod="put_object", 
+    url = boto3.client("s3").generate_presigned_url(
+        ClientMethod="put_object",
         Params={
-            'Bucket': bucket_name[5:], 
-            'Key': path_to_upload_file + filename, 
-            'Metadata': tags or dict()
+            "Bucket": bucket_name[5:],
+            "Key": path_to_upload_file + filename,
+            "Metadata": tags or dict()
         }, 
         ExpiresIn=3600,
     )
@@ -96,8 +88,6 @@ def lambda_handler(event, context):
         verified_token = cognito_utils.verify_cognito_token(token)
     except Exception as e:
         logger.info(f"Authentication error: {e}")
-
-
     if not verified_token:
         logger.info("Supplied token could not be verified")
         return {
@@ -111,7 +101,6 @@ def lambda_handler(event, context):
         }
     filename = event["queryStringParameters"]["filename"]
     url = _generate_signed_upload_url(filename, tags=event["queryStringParameters"])
-    
     if url is None:
         return {
             "statusCode": 400,
@@ -120,5 +109,4 @@ def lambda_handler(event, context):
                 "file name matches mission file naming conventions."
              )
         }
-    
     return {"statusCode": 200, "body": json.dumps(url)}
