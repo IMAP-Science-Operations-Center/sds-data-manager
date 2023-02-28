298c09bc8b90b0de05dd7f7940e971ef02d3bc66
Bryan Harter <harter864@gmail.com>
2023-02-28 16:32:50 -0700

    fixing the upload_api formatting issues

--- sds_data_manager/lambda_code/SDSCode/upload_api.py
+++ sds_data_manager/lambda_code/SDSCode/upload_api.py
@@ -1,14 +1,19 @@
 import json
+import logging
 import os
 import boto3
 import cognito_utils
 
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.INFO)
+
 def _load_allowed_filenames():
-    '''
+    """
     This function loads the config.json file, and stores the contents as a python dictionary 
     
     :return: dictionary object of file types and their attributes. 
-    '''
+    """
     current_dir = os.path.dirname(__file__)
     config_file = os.path.join(current_dir, "config.json")
     
@@ -17,14 +22,14 @@ def _load_allowed_filenames():
     return data
 
 def _check_for_matching_filetype(pattern, filename):
-    '''
+    """
     This function takes in a pattern from config.json and compares it to the desired file name
 
     :param pattern: A file naming pattern from the config.json
     :param filename: Required.  String name of the desired file name.  
     
     :return: None if there is no match, or the file_dictionary if there is.  
-    '''
+    """
     split_filename = filename.replace("_", ".").split(".")
 
     if len(split_filename) != len(pattern):
@@ -44,18 +49,18 @@ def _check_for_matching_filetype(pattern, filename):
     return file_dictionary
 
 def _generate_signed_upload_url(filename, tags={}):
-    '''
+    """
     Based on a given filename, this function will open up a presigned url into the correct location on the SDS storage bucket
     
     :param filename: Required.  A string representing the name of the object to upload.  
     :param tags: Optional.  A dictionary object of key:value pairs that will be stored in the S3 object metadata.  
 
     :return: None if the filename does not match mission naming conventions.  Otherwise, a URL string.  
-     '''
+    """
     filetypes = _load_allowed_filenames()
     for filetype in filetypes:
-        path_to_upload_file = filetype['path']
-        metadata = _check_for_matching_filetype(filetype['pattern'], filename)
+        path_to_upload_file = filetype["path"]
+        metadata = _check_for_matching_filetype(filetype["pattern"], filename)
         if metadata is not None:
             break
         
@@ -64,16 +69,18 @@ def _generate_signed_upload_url(filename, tags={}):
         return None
     
     bucket_name = os.environ["S3_BUCKET"]
-    url = boto3.client('s3').generate_presigned_url(ClientMethod='put_object', 
-                                                    Params={'Bucket': bucket_name[5:], 
-                                                    'Key': path_to_upload_file+filename, 
-                                                    'Metadata': tags}, 
-                                                    ExpiresIn=3600)
+    url = boto3.client('s3').generate_presigned_url(
+        ClientMethod="put_object", 
+        Params={'Bucket': bucket_name[5:], 
+                'Key': path_to_upload_file + filename, 
+                'Metadata': tags or dict()}, 
+        ExpiresIn=3600,
+    )
     
     return url
 
 def lambda_handler(event, context):
-    '''
+    """
     The entry point to the upload API lambda.  
     This function returns an S3 signed-URL based on the input filename, which the user can 
     then use to load a file onto the SDS. 
@@ -84,7 +91,7 @@ def lambda_handler(event, context):
     :param context: Unused
     
     :return: If all checks are successful, this returns a pre-signed url where users can upload a data file to the SDS.  
-    '''
+    """
     verified_token = False
     try:
         token=event["headers"]["authorization"]
@@ -96,27 +103,26 @@ def lambda_handler(event, context):
     if not verified_token:
         logger.info("Supplied token could not be verified")
         return {
-                'statusCode': 400,
-                'body': json.dumps("Supplied token could not be verified")
+                "statusCode": 400,
+                "body": json.dumps("Supplied token could not be verified")
             }
 
-    if 'filename' not in event['queryStringParameters']:
+    if "filename" not in event["queryStringParameters"]:
         return {
-            'statusCode': 400,
-            'body': json.dumps("Please specify a filename to upload")
+            "statusCode": 400,
+            "body": json.dumps("Please specify a filename to upload")
         }
         
-    filename = event['queryStringParameters']['filename']
-    url = _generate_signed_upload_url(filename, tags=event['queryStringParameters'])
+    filename = event["queryStringParameters"]["filename"]
+    url = _generate_signed_upload_url(filename, tags=event["queryStringParameters"])
     
     if url is None:
         return {
-            'statusCode': 400,
-            'body': json.dumps("A pre-signed URL could not be generated. Please ensure that the file name matches mission file naming conventions.")
+            "statusCode": 400,
+            "body": json.dumps(
+                "A pre-signed URL could not be generated. Please ensure that the "
+                "file name matches mission file naming conventions."
+             )
         }
     
-        
-    return {
-        'statusCode': 200,
-        'body': json.dumps(url)
-    }
+    return {"statusCode": 200, "body": json.dumps(url)}
