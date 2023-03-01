554614628c11e6a7f2ee926e3c2399eac5c124ad
Bryan Harter <harter864@gmail.com>
2023-03-01 14:36:00 -0700

    A few more fixes

--- sds_data_manager/sds_data_manager_stack.py
+++ sds_data_manager/sds_data_manager_stack.py
@@ -15,8 +15,7 @@ from constructs import Construct
 
 class SdsDataManagerStack(Stack):
     def __init__(
-        self, scope: Construct, construct_id: str, sds_id: str, 
-        userpool_id: str='', app_client_id: str='', **kwargs
+        self, scope: Construct, construct_id: str, sds_id: str, **kwargs
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
@@ -157,11 +156,7 @@ class SdsDataManagerStack(Stack):
             runtime=lambda_.Runtime.PYTHON_3_9,
             timeout=cdk.Duration.minutes(15),
             memory_size=1000,
-            environment={
-                "S3_BUCKET": data_bucket.s3_url_for_object(),
-                "COGNITO_USERPOOL_ID": userpool_id, 
-                "COGNITO_APP_ID": app_client_id,
-            },
+            environment={"S3_BUCKET": data_bucket.s3_url_for_object()},
         )
         upload_api_lambda.add_to_role_policy(s3_write_policy)
         upload_api_lambda.apply_removal_policy(cdk.RemovalPolicy.DESTROY)
@@ -189,8 +184,6 @@ class SdsDataManagerStack(Stack):
                 "OS_DOMAIN": sds_metadata_domain.domain_endpoint,
                 "OS_PORT": "443",
                 "OS_INDEX": "metadata",
-                "COGNITO_USERPOOL_ID": userpool_id, 
-                "COGNITO_APP_ID": app_client_id,
             },
         )
         query_api_lambda.add_to_role_policy(opensearch_read_only_policy)
@@ -216,10 +209,6 @@ class SdsDataManagerStack(Stack):
             handler="lambda_handler",
             runtime=lambda_.Runtime.PYTHON_3_9,
             timeout=cdk.Duration.seconds(60),
-            environment={
-                "COGNITO_USERPOOL_ID": userpool_id, 
-                "COGNITO_APP_ID": app_client_id,
-            },
         )
         download_query_api.add_to_role_policy(opensearch_all_http_permissions)
         download_query_api.add_to_role_policy(s3_read_policy)
