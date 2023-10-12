d78c76f5dc4069d7de3191cbc044e96920a4ef0f
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-10-12 09:37:28 -0600

    updated docstring

--- sds_data_manager/utils/get_dependency.py
+++ sds_data_manager/utils/get_dependency.py
@@ -3,7 +3,19 @@ import sys
 from pathlib import Path
 
 def get_dependency(key):
+    """
+    Retrieves dependencies for each instrument/level.
 
+    Parameters
+    ----------
+    key : str
+        Target name
+
+    Returns
+    -------
+    packets : list
+        List of all the dependencies
+    """
     dependency_path=Path(sys.modules[__name__.split(
         '.')[0]].__file__).parent / 'utils' / 'dependencies.json'
 
