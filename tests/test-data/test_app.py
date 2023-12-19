ce81504091847f85e665b3369671b91d677e3d2a
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-12-19 09:55:07 -0700

    updates

--- tests/test-data/test_app.py
+++ tests/test-data/test_app.py
@@ -1,11 +1,22 @@
+"""
+A simple Flask web application designed to be Dockerized
+and deployed on an EC2 instance. Intended for verifying
+the successful deployment and operation in an
+ECR and EC2 setup. The application listens on
+all interfaces (0.0.0.0) at port 8080,
+allowing external access for testing.
+"""
 from flask import Flask
 
+# Create a Flask application
 app = Flask(__name__)
 
 
+# Decorator that tells Flask what URL
+# should trigger the function that follows.
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World."
 
 
 if __name__ == "__main__":
