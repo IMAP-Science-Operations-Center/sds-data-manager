dc1ea3cbd3cc4b74a754dc7b5d4b5b475322684d
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-01-16 12:31:11 -0700

    updates to cli

--- tests/docker_images/imap_api.py
+++ tests/docker_images/imap_api.py
@@ -1,8 +1,9 @@
-"""Module for the IMAP API utilities.
+"""Module for testing API utilities.
 """
 import argparse
 import logging
 import os
+
 import requests
 
 logging.basicConfig(level=logging.INFO)
@@ -18,15 +19,13 @@ def _parse_args():
     """
 
     description = (
-        "This command line program invokes the downloads"
-        "an file from the s3_uri, modifies it, and uploads"
+        "This command line program downloads"
+        "a file from the s3_uri, modifies it, and uploads"
         "the modified file to the same s3 uri. "
-        "Example usage: python imap_api.py <s3_uri> <local_file_location>. "
+        "Example usage: python imap_api.py <s3_uri>. "
     )
 
-    s3_uri_help = (
-        "The s3_uri from which to download. "
-    )
+    s3_uri_help = "The s3_uri from which to download. "
 
     api_endpoint_help = (
         "The api_endpoint. Default is https://api.dev.imap-mission.com. "
@@ -55,32 +54,33 @@ def download(s3_uri, api_endpoint="https://api.dev.imap-mission.com"):
     Returns
     -------
     file_name_and_path : str
-        The local file path where the downloaded file is saved.
+        The file path where the downloaded file is saved.
     """
     logging.info(f"Starting download from S3 URI: {s3_uri}")
 
-    url_with_parameters = api_endpoint + "/download?" + s3_uri
+    url_with_parameters = f"{api_endpoint}/download?{s3_uri}"
     response = requests.get(url_with_parameters)
 
     # Check if running in Docker
-    running_in_docker = os.getenv('DOCKER_ENV') is not None
+    running_in_docker = os.getenv("DOCKER_ENV") is not None
 
     # Set the base directory
     base_directory = "/mnt/data" if running_in_docker else "."
 
-    # Now join the base directory with the file name
-    file_name_and_path = os.path.join(base_directory, s3_uri[5:])
+    # Join the base directory with the file name
+    file_name_and_path = os.path.join(base_directory, s3_uri.replace("s3://", ""))
 
     download_dir = os.path.dirname(file_name_and_path)
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
 
     with open(file_name_and_path, "wb") as file:
-       file.write(response.content)
+        file.write(response.content)
 
     logging.info(f"File downloaded and saved to: {file_name_and_path}")
 
     return file_name_and_path
+
 
 def upload(local_file_location, api_endpoint="https://api.dev.imap-mission.com"):
     """
@@ -89,41 +89,29 @@ def upload(local_file_location, api_endpoint="https://api.dev.imap-mission.com")
     Parameters
     ----------
     local_file_location : str
-        The local file path of the file to be uploaded.
+        The file path of the file to be uploaded.
     api_endpoint : str, optional
         The API endpoint to use for uploading the file.
-
-    Returns
-    -------
-    put_response : requests.Response
-        The response object from the PUT request to upload the file.
     """
     logging.info(f"Starting upload for file: {local_file_location}")
 
     remote_file_name = os.path.basename(local_file_location)
 
-    # arbitrarily modify file name
-    base, ext = remote_file_name.rsplit('.', 1)
-    modified_file_name = f"{base}_mod.{ext}"
-
+    # Modify descriptor in file name
+    modified_file_name = remote_file_name.replace(
+        remote_file_name[12:15], f"{remote_file_name[12:15]}-test"
+    )
+    # Upload the file
     url_with_parameters = f"{api_endpoint}/upload?filename={modified_file_name}"
-
     get_response = requests.get(url_with_parameters)
-
     upload_url = get_response.json()
-
-    with open(local_file_location, "rb") as object_file:
-        object_text = object_file.read()
-
-    requests.put(upload_url, data=object_text)
+    requests.put(upload_url)
 
     logging.info(f"File uploaded: {modified_file_name}")
-    return
 
 
 def main():
-    """Main function for the IMAP API utilities.
-    """
+    """Main function for the IMAP API utilities."""
     args = _parse_args()
 
     if args.api_endpoint is not None:
