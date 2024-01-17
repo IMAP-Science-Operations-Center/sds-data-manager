cb4bb87458dd3a835c59d77f813ae3cc775fe0eb
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2024-01-17 16:16:50 -0700

    added status tracking table and removed instrument tables

--- sds_data_manager/lambda_code/SDSCode/database/models.py
+++ sds_data_manager/lambda_code/SDSCode/database/models.py
@@ -1,6 +1,58 @@
 """Main file to store schema definition"""
-from sqlalchemy import Boolean, Column, DateTime, Identity, Integer, String
+import enum
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Enum,
+    ForeignKey,
+    Identity,
+    Integer,
+    String,
+)
 from sqlalchemy.orm import DeclarativeBase
+
+
+class InstrumentEnum(enum.Enum):
+    """Instrument table enums"""
+
+    CODICE = "codice"
+    GLOWS = "glows"
+    HI45 = "hi-45"
+    HI90 = "hi-90"
+    HIT = "hit"
+    IDEX = "idex"
+    LO = "lo"
+    MAG = "mag"
+    SWAPI = "swapi"
+    SWE = "swe"
+    ULTRA45 = "ultra-45"
+    ULTRA90 = "ultra-90"
+
+
+class DataLevelEnum(enum.Enum):
+    L0 = "l0"
+    L1A = "l1a"
+    L1B = "l1b"
+    L1C = "l1c"
+    L1CA = "l1ca"
+    L1CB = "l1cb"
+    L1D = "l1d"
+    L2 = "l2"
+    L2PRE = "l2pre"
+    L3 = "l3"
+    L3A = "l3a"
+    L3B = "l3b"
+    L3C = "l3c"
+    L3D = "l3d"
+
+
+class StatusEnum(enum.Enum):
+    """File status enums"""
+
+    SUCCESS = 1
+    FAILURE = 0
 
 
 class Base(DeclarativeBase):
@@ -24,82 +76,31 @@ class UniversalSpinTable(Base):
     repointing_number = Column(Integer, nullable=False)
 
 
-class FileCatalogTable:
-    """Common file catalog table"""
+class StatusTrackingTable(Base):
+    """Status tracking table"""
+
+    __tablename__ = "status_tracking_table"
+
+    id = Column(Integer, Identity(start=1, increment=1), primary_key=True)
+    file_to_create_path = Column(String, nullable=False)
+    status = Column(Enum(StatusEnum), nullable=False)
+    job_definition = Column(String, nullable=False)
+    ingestion_date = Column(DateTime(timezone=True), nullable=True)
+
+
+class FileCatalogTable(Base):
+    """File catalog table"""
+
+    __tablename__ = "file_catalog_table"
 
     # TODO: determine cap for strings
     id = Column(Integer, Identity(start=1, increment=1), primary_key=True)
     file_path = Column(String, nullable=False)
-    instrument = Column(String(6), nullable=False)
-    data_level = Column(String(3), nullable=False)
+    instrument = Column(Enum(InstrumentEnum), nullable=False)
+    data_level = Column(Enum(DataLevelEnum), nullable=False)
     descriptor = Column(String, nullable=False)
     start_date = Column(DateTime(timezone=True), nullable=False)
     end_date = Column(DateTime(timezone=True), nullable=False)
-    ingestion_date = Column(DateTime(timezone=True), nullable=False)
     version = Column(String, nullable=False)
     extension = Column(String, nullable=False)
-
-
-# TODO: Follow-up PR should add in columns for each instrument
-# for instrument dependency IDs, SPICE ID, parent id,
-# and pointing id
-
-
-class LoTable(FileCatalogTable, Base):
-    """IMAP-Lo File Catalog Table"""
-
-    __tablename__ = "lo"
-
-
-class HiTable(FileCatalogTable, Base):
-    """IMAP-Hi File Catalog Table"""
-
-    __tablename__ = "hi"
-
-
-class UltraTable(FileCatalogTable, Base):
-    """IMAP-Ultra File Catalog Table"""
-
-    __tablename__ = "ultra"
-
-
-class HITTable(FileCatalogTable, Base):
-    """HIT File Catalog Table"""
-
-    __tablename__ = "hit"
-
-
-class IDEXTable(FileCatalogTable, Base):
-    """IDEX File Catalog Table"""
-
-    __tablename__ = "idex"
-
-
-class SWAPITable(FileCatalogTable, Base):
-    """SWAPI File Catalog Table"""
-
-    __tablename__ = "swapi"
-
-
-class SWETable(FileCatalogTable, Base):
-    """SWE File Catalog Table"""
-
-    __tablename__ = "swe"
-
-
-class CoDICETable(FileCatalogTable, Base):
-    """CoDICE File Catalog Table"""
-
-    __tablename__ = "codice"
-
-
-class MAGTable(FileCatalogTable, Base):
-    """MAG File Catalog Table"""
-
-    __tablename__ = "mag"
-
-
-class GLOWSTable(FileCatalogTable, Base):
-    """GLOWS File Catalog Table"""
-
-    __tablename__ = "glows"
+    status_tracking_id = Column(Integer, ForeignKey("status_tracking_table.id"))
