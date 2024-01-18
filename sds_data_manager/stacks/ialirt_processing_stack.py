e7219764e9a2ebf307f94036cb40391f0f3eb1ea
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-01-18 10:54:02 -0700

    update to organization

--- sds_data_manager/stacks/ialirt_processing_stack.py
+++ sds_data_manager/stacks/ialirt_processing_stack.py
@@ -6,11 +6,8 @@ from aws_cdk import RemovalPolicy, Stack
 from aws_cdk import aws_dynamodb as dynamodb
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecr as ecr
+from aws_cdk import aws_iam as iam
 from constructs import Construct
-
-from sds_data_manager.constructs.ialirt_compute_resources import (
-    IalirtEC2Resources,
-)
 
 
 class IalirtProcessing(Stack):
@@ -22,6 +19,7 @@ class IalirtProcessing(Stack):
         construct_id: str,
         vpc: ec2.Vpc,
         repo: ecr.Repository,
+        instance_type: str = "t3.micro",
         **kwargs,
     ) -> None:
         """Constructor
@@ -41,17 +39,77 @@ class IalirtProcessing(Stack):
 
         self.vpc = vpc
         self.repo = repo
+        self.instance_type = instance_type
         self.add_compute_resources()
         self.add_dynamodb_table()
 
     # Setup the EC2 resources
     def add_compute_resources(self):
-        """Add EC2 components"""
-        self.ec2_resources = IalirtEC2Resources(
+        """EC2 compute environment."""
+
+        # Define user data script
+        # - Updates the instance
+        # - Installs Docker
+        # - Starts the Docker
+        # - Logs into AWS ECR to pull the image onto the instance
+        # - Pulls the Docker image
+        # - Runs the Docker container
+        user_data = ec2.UserData.for_linux()
+        user_data.add_commands(
+            "yum update -y",
+            "amazon-linux-extras install docker -y",
+            "systemctl start docker",
+            "systemctl enable docker",
+            "$(aws ecr get-login --no-include-email --region us-west-2 | bash)",
+            f"docker pull {self.repo.repository_uri}:latest",
+            f"docker run --rm -d -p 8080:8080 {self.repo.repository_uri}:latest",
+        )
+
+        # Security Group for the EC2 Instance
+        security_group = ec2.SecurityGroup(
             self,
-            "IalirtEC2Environment",
+            "IalirtEC2SecurityGroup",
             vpc=self.vpc,
-            repo=self.repo,
+            description="Security group for Ialirt EC2 instance",
+        )
+
+        # Allow ingress to LASP IP address range and specific port
+        security_group.add_ingress_rule(
+            ec2.Peer.ipv4("128.138.131.0/24"),
+            ec2.Port.tcp(8080),
+            "Allow inbound traffic on TCP port 8080",
+        )
+
+        # Create an IAM role for the EC2 instance
+        # - Read-only access to AWS ECR
+        # - Basic instance management via AWS Systems Manager
+        # Note: the Systems Manager provides a secure way for
+        # users to interact and access the EC2 during development.
+        ec2_role = iam.Role(
+            self,
+            "IalirtEC2Role",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AmazonEC2ContainerRegistryReadOnly"
+                ),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AmazonSSMManagedInstanceCore"
+                ),
+            ],
+        )
+
+        # Create an EC2 instance
+        ec2.Instance(
+            self,
+            "IalirtEC2Instance",
+            instance_type=ec2.InstanceType(self.instance_type),
+            machine_image=ec2.MachineImage.latest_amazon_linux2(),
+            vpc=self.vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            security_group=security_group,
+            role=ec2_role,
+            user_data=user_data,
         )
 
     # I-ALiRT IOIS DynamoDB
@@ -60,7 +118,7 @@ class IalirtProcessing(Stack):
     # src-seq-ctr: increments with each packet (included in filename?)
     # ccsds-filename: filename of the packet
     def add_dynamodb_table(self):
-        """Add DynamoDB Table here"""
+        """DynamoDB Table."""
         dynamodb.Table(
             self,
             "DynamoDB-ialirt",
