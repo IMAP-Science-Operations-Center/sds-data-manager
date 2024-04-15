d501d173adbeb115daf8b8a6769059eb79d50d2c
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-04-15 17:14:21 -0600

    ialirt test

--- tests/infrastructure/test_ialirt_processing_stack.py
+++ tests/infrastructure/test_ialirt_processing_stack.py
@@ -1,37 +1,37 @@
-"""Verify the successful deployment and operation in an ECR and EC2 setup.
-
-# TODO: this is not ideal, but it works until we setup ECS.
-Steps for setting up test:
-1. Deploy IalirtProcessing Stack
-2. While in ialirt_ec2 directory build Docker image and
-push to ECR. Follow the instructions in the Dockerfile.
-3. Navigate to EC2 instance in the AWS Console, check
-the box next to it and click Connect.
-Connect using Session Manager.
-4. Make certain you are logged in to the LASP VPN.
-5. Run the following commands:
-    a.  'sudo docker ps'
-    b. if a container is not yet running:
-    'sudo docker pull <repo uri>:latest'
-    c. 'sudo docker run --rm -d -p 8080:8080 <repo uri>:latest'
-6.  If you get a permissions error:
-    'aws ecr get-login-password --region <region> |
-    sudo docker login --username AWS --password-stdin <repo uri>'
-
-Note: verification may also be done via the
-webbrowser: http://<EC2_IP>:8080/
-"""
-
-import os
-
+"""Tests I-ALiRT processing stack."""
+import boto3
 import requests
 
-# Environment variable for EC2 IP Address (set manually)
-EC2_IP = os.getenv("EC2_IP_ADDRESS")
+from sds_data_manager import (
+    IALIRT_PORTS_TO_ALLOW_CONTAINER_1,
+)
 
 
-def test_flask_app_response():
-    """Test the Flask application response."""
-    if EC2_IP is not None:  # pragma: no cover
-        url = f"http://{EC2_IP}:8080/"
-        requests.get(url, timeout=10)
+def get_alb_dns(stack_name, port, container_name):
+    """Retrieve DNS for the ALB from CloudFormation."""
+    client = boto3.client("cloudformation")
+    response = client.describe_stacks(StackName=stack_name)
+    output_key = f"LoadBalancerDNS{container_name}{port}"
+    for output in response["Stacks"][0]["Outputs"]:
+        if output["OutputKey"] == output_key:
+            return output["OutputValue"]
+    raise ValueError(f"DNS output not found for port {port} in stack.")
+
+
+def test_alb_response_container():
+    """Test to ensure the ALB responds with HTTP 200 status."""
+    stack_name = "IalirtProcessing7"
+    containers = {
+        "Container1": IALIRT_PORTS_TO_ALLOW_CONTAINER_1,
+        # "Container2": IALIRT_PORTS_TO_ALLOW_CONTAINER_2,
+    }
+
+    for container_name, ports in containers.items():
+        for port in ports:
+            alb_dns = get_alb_dns(stack_name, port, container_name)
+            print(f"Testing URL: {alb_dns}")
+            # Specify a timeout for the request
+            response = requests.get(alb_dns, timeout=10)  # timeout in seconds
+            assert (
+                response.status_code == 200
+            ), f"ALB did not return HTTP 200 on port {port} for {container_name}"
