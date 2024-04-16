c68d5abec9d0f4d2c77dd92d729b20e1edc63d17
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-04-16 09:10:20 -0600

    single container

---
+++ sds_data_manager/__init__.py
@@ -0,0 +1,8 @@
+"""sds-data-manager processing package.
+
+This package contains the IMAP architecture.
+"""
+
+# Ports configuration for various containers within the IALiRT system
+IALIRT_PORTS_TO_ALLOW_CONTAINER_1 = [8080, 8081]
+IALIRT_PORTS_TO_ALLOW_CONTAINER_2 = [8082, 8083]
