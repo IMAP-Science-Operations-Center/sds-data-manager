7568e036621209c040d920b870594e1e9033d53b
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-06-20 16:18:07 -0600

    working copy

--- tests/test-data/ialirt_ec2_primary/test_app.py
+++ tests/test-data/ialirt_ec2_primary/test_app.py
@@ -2,9 +2,39 @@
 
 import os
 
+import boto3
 from flask import Flask
 
 app = Flask(__name__)
+
+# Configure the S3 client
+s3 = boto3.client("s3")
+
+# Set the upload folder and allowed extensions
+UPLOAD_FOLDER = "/mnt/s3"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# Define the bucket name and file details
+sts_client = boto3.client("sts")
+identity = sts_client.get_caller_identity()
+account = identity["Account"]
+bucket_name = f"sds-data-{account}"
+file_name = "test_file_primary.txt"
+local_file_path = os.path.join(UPLOAD_FOLDER, file_name)
+
+
+def create_and_upload_file():
+    """Create and upload a file."""
+    # TODO: this will be replaced by an upload api
+    os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+    with open(local_file_path, "w") as file:
+        file.write("Hello, this is a test file.")
+
+    s3.upload_file(local_file_path, bucket_name, file_name)
+    print(
+        f"File {file_name} created and uploaded to S3 bucket "
+        f"{bucket_name} successfully."
+    )
 
 
 @app.route("/")
@@ -21,4 +51,5 @@ def list_files():
 
 
 if __name__ == "__main__":
+    create_and_upload_file()
     app.run(host="0.0.0.0", port=8080)
