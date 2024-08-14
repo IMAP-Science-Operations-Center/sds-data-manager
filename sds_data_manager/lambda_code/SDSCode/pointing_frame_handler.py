e22594bfe0f5f0c98f3db63d92023dc05ffd77bb
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-08-14 13:20:12 -0600

    code updates

--- sds_data_manager/lambda_code/SDSCode/pointing_frame_handler.py
+++ sds_data_manager/lambda_code/SDSCode/pointing_frame_handler.py
@@ -1,5 +1,10 @@
 """Generate Pointing Frame.
 
+Notes
+----------
+Kernels that are required to run this code:
+1. 
+
 References
 ----------
 https://spiceypy.readthedocs.io/en/main/documentation.html.
@@ -46,6 +51,7 @@ def get_coverage(ck_kernel):
     # Get the coverage window
     # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.ckcov
     cover = spice.ckcov(ck_kernel, int(id_imap_spacecraft), True, "SEGMENT", 0, "TDB")
+    # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.wnfetd
     et_start, et_end = spice.wnfetd(cover, 0)
     et_times = np.arange(et_start, et_end, STEP)
 
@@ -65,6 +71,7 @@ def average_quaternions(et_times):
     q_avg : np.array
         Average quaternion.
     z_eclip_time : list
+        Z-axis of the ECLIPJ2000 frame. Used for plotting.
     """
     body_quats = []
     z_eclip_time = []
@@ -72,8 +79,10 @@ def average_quaternions(et_times):
 
     for tdb in et_times:
         # Rotation matrix from IMAP spacecraft frame to ECLIPJ2000.
+        # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.pxform
         body_rots = spice.pxform("IMAP_SPACECRAFT", "ECLIPJ2000", tdb)
         # Convert rotation matrix to quaternion.
+        # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.m2q
         body_quat = spice.m2q(body_rots)
         body_quats.append(body_quat)
         # z-axis of the ECLIPJ2000 frame.
@@ -119,7 +128,7 @@ def create_rotation_matrix(et_times):
     rotation_matrix : np.array
         Rotation matrix.
     z_avg : np.array
-        Inertial z axis.
+        Inertial z axis. Used for plotting.
     """
     # Averaged quaternions.
     q_avg, _ = average_quaternions(et_times)
@@ -127,6 +136,7 @@ def create_rotation_matrix(et_times):
     # TODO: Ask Nick if this part is necessary.
     # Converts the averaged quaternion (q_avg) into a rotation matrix
     # and get inertial z axis.
+    # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.q2m
     z_avg = spice.q2m(list(q_avg))[:, 2]
     # y_avg is perpendicular to both z_avg and the standard Z-axis.
     y_avg = np.cross(z_avg, [0, 0, 1])
@@ -140,42 +150,61 @@ def create_rotation_matrix(et_times):
 
 
 def create_pointing_frame():
-    """Create the pointing frame."""
-    mount_path = Path(os.getenv("EFS_MOUNT_PATH"))
-    kernels = [str(file) for file in mount_path.iterdir()]
-    ck_kernel = [str(file) for file in mount_path.iterdir() if file.suffix == ".bc"]
+    """Create the pointing frame.
 
+    Returns
+    -------
+    path_to_imap_dps : str
+        Path to dps frame.
+    """
+    # Mount path to EFS.
+    mount_path = Path(os.getenv("EFS_MOUNT_PATH"))
+
+    # TODO: this part will change with ensure_spice decorator.
+    kernels = [str(file) for file in mount_path.iterdir()]
+    ck_kernel = [str(file) for file in mount_path.iterdir()
+                 if file.suffix == ".bc"]
+
+    # Furnish the kernels.
     with spice.KernelPool(kernels):
+
+        # Get timerange for the pointing frame kernel.
         et_start, et_end, et_times = get_coverage(str(ck_kernel[0]))
+        # Create a rotation matrix
         rotation_matrix, _ = create_rotation_matrix(et_times)
 
-        # Convert the rotation matrix to a quaternion
+        # Convert the rotation matrix to a quaternion.
+        # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.m2q
         q_avg = spice.m2q(rotation_matrix)
 
         # TODO: come up with naming convention.
         path_to_imap_dps = mount_path / "imap_dps.bc"
 
+        # Open a new CK file, returning the handle of the opened file.
+        # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.ckopn
         handle = spice.ckopn(str(path_to_imap_dps), "CK", 0)
+        # Get the SCLK ID.
+        # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.gipool
         id_imap_sclk = spice.gipool("CK_-43000_SCLK", 0, 1)
-        sclk_begtim = spice.sce2c(
-            int(id_imap_sclk), et_start
-        )  # Convert start time to SCLK
+        # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.sce2c
+        # Convert start and end times to SCLK.
+        sclk_begtim = spice.sce2c(int(id_imap_sclk), et_start)
         sclk_endtim = spice.sce2c(int(id_imap_sclk), et_end)
 
-        et_start1 = sclk_begtim
-        et_end1 = sclk_endtim
+        # Get the pointing frame ID.
+        # https://spiceypy.readthedocs.io/en/main/documentation.html#spiceypy.spiceypy.gipool
         id_imap_dps = spice.gipool("FRAME_IMAP_DPS", 0, 1)
 
         spice.ckw02(
             handle,
-            et_start1,  # Single start time
-            et_end1,  # Single stop time
+            sclk_begtim,  # Single start time
+            sclk_endtim,  # Single stop time
             int(id_imap_dps),  # Instrument ID
             "ECLIPJ2000",  # Reference frame
             "IMAP_DPS",  # Segment identifier
             1,  # Only one record
-            np.array([et_start1]),  # Single start time
-            np.array([et_end1]),  # Single stop time
+            np.array([sclk_begtim]),  # Single start time
+            np.array([sclk_endtim]),  # Single stop time
             q_avg,  # quaternion
             np.array([0.0, 0.0, 0.0]),  # Single angular velocity vector
             np.array([1.0]),  # RATES (seconds per tick)
