0bbc72e14d14a6b79f8ea85c5e13ffb3caba2c17
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-11-01 09:19:47 -0600

    first pass

---
+++ sds_data_manager/constructs/ialirt_api_manager_construct.py
@@ -0,0 +1,109 @@
+"""Configure the SDS API Manager."""
+
+import aws_cdk as cdk
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_lambda as lambda_
+from constructs import Construct
+
+from .api_gateway_construct import ApiGateway
+
+
+class IalirtApiManager(Construct):
+    """Construct for API Management."""
+
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        code: lambda_.Code,
+        api: ApiGateway,
+        env: cdk.Environment,
+        data_bucket,
+        vpc,
+        layers: list,
+        **kwargs,
+    ) -> None:
+        """Initialize the SdsApiManagerConstruct.
+
+        Parameters
+        ----------
+        scope : obj
+            Parent construct
+        construct_id : str
+            A unique string identifier for this construct
+        code : lambda_.Code
+            Lambda code bundle
+        api : obj
+            The APIGateway stack
+        env : obj
+            The CDK environment
+        data_bucket : obj
+            The data bucket
+        vpc : obj
+            The VPC
+        layers : list
+            List of Lambda layers arns
+        kwargs : dict
+            Keyword arguments
+        """
+        super().__init__(scope, construct_id, **kwargs)
+
+        s3_read_policy = iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=["s3:GetObject"],
+            resources=[
+                f"{data_bucket.bucket_arn}/*",
+            ],
+        )
+
+        # query API lambda
+        query_api_lambda = lambda_.Function(
+            self,
+            id="QueryAPILambda",
+            function_name="query-api-handler",
+            code=code,
+            handler="SDSCode.query_api.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            timeout=cdk.Duration.minutes(1),
+            memory_size=1000,
+            allow_public_subnet=True,
+            vpc=vpc,
+            environment={
+                "S3_BUCKET": data_bucket.bucket_name,
+                "REGION": env.region,
+            },
+            layers=layers,
+            architecture=lambda_.Architecture.ARM_64,
+        )
+
+        api.add_route(
+            route="query",
+            http_method="GET",
+            lambda_function=query_api_lambda,
+        )
+
+        # download API lambda
+        download_api = lambda_.Function(
+            self,
+            id="DownloadAPILambda",
+            function_name="download-api-handler",
+            code=code,
+            handler="SDSCode.download_api.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            timeout=cdk.Duration.minutes(1),
+            environment={
+                "S3_BUCKET": data_bucket.bucket_name,
+                "REGION": env.region,
+            },
+            layers=layers,
+            architecture=lambda_.Architecture.ARM_64,
+        )
+
+        download_api.add_to_role_policy(s3_read_policy)
+
+        api.add_route(
+            route="download",
+            http_method="GET",
+            lambda_function=download_api,
+            use_path_params=True,
+        )
