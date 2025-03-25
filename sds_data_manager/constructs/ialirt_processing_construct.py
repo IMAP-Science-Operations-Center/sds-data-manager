4e897bf154f55f4646b6882d1c14537c27b53482
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-03-25 08:57:05 -0600

    Add IP address to security group for Kiel

--- sds_data_manager/constructs/ialirt_processing_construct.py
+++ sds_data_manager/constructs/ialirt_processing_construct.py
@@ -81,7 +81,7 @@ class IalirtProcessing(Construct):
 
         # Allow inbound and outbound traffic from a specific port and IP.
         # IPs: LASP IP, BlueNet (tlm relay)
-        ip_ranges = ["128.138.131.0/24", "198.118.1.14/32"]
+        ip_ranges = ["128.138.131.0/24", "198.118.1.14/32", "193.174.22.3/32"]
         for port in self.ports:
             for ip_range in ip_ranges:
                 self.ecs_security_group.add_ingress_rule(
