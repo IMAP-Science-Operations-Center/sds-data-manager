66c2453998e9125e5b3ec08fe91c0d3caecbe84a
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-06-17 08:39:30 -0600

    update to tests

--- tests/lambda_endpoints/test_ialirt_ingest.py
+++ tests/lambda_endpoints/test_ialirt_ingest.py
@@ -260,9 +260,10 @@ def test_process_algorithms(mock_swe, mock_hit, setup_dynamodb):
     )
 
 
-@patch("imap_processing.spice.time.str_to_et", return_value=123456789.0)
+@patch("spiceypy.str2et", return_value=123456789.0)
+@patch("spiceypy.furnsh")
 @patch("sds_data_manager.lambda_code.IAlirtCode.ialirt_ingest.requests.get")
-def test_get_latest_spice_kernels(mock_get, mock_str_to_et):
+def test_get_latest_spice_kernels(mock_get, mock_furnsh, mock_str2et):
     """Test get_latest_spice_kernels function."""
     mock_files = [
         "imap_sclk_0000.tsc",
