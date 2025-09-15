f27d2973d25da93e03e0a55df350c59dd0caa26b
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-09-15 13:12:45 -0600

    change timeout for test

--- sds_data_manager/constructs/ialirt_ingest_lambda_construct.py
+++ sds_data_manager/constructs/ialirt_ingest_lambda_construct.py
@@ -166,7 +166,7 @@ class IalirtIngestLambda(Construct):
                 file="IAlirtCode/Dockerfile.ingest",
             ),
             function_name="ialirt-ingest",
-            timeout=cdk.Duration.minutes(4),
+            timeout=cdk.Duration.minutes(10),
             memory_size=1000,
             role=lambda_role,
             vpc=self.vpc,
