7553713b662eab121bf66eea6672b6b3456190ce
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-10-17 13:30:57 -0600

    update construct

--- tests/infrastructure/test_ialirt_ingest_lambda_construct.py
+++ tests/infrastructure/test_ialirt_ingest_lambda_construct.py
@@ -30,6 +30,66 @@ def populate_algorithm_table(setup_dynamodb):
     return items
 
 
+@pytest.fixture
+def populate_data_table(setup_data_table):
+    """Populate DynamoDB table."""
+    data_table = setup_data_table["data_table"]
+    items = [
+        {
+            "instrument": "mag",
+            "met_in_utc": "2021-01-01T00:00:00",
+            "last_modified": "2021-01-01T00:00:00",
+            "data_product_1": str(1234.56),
+        },
+        {
+            "instrument": "mag",
+            "met_in_utc": "2021-02-01T00:00:00",
+            "last_modified": "2021-02-01T00:00:00",
+            "data_product_2": str(101.3),
+        },
+    ]
+    for item in items:
+        data_table.put_item(Item=item)
+
+    return items
+
+
+def test_data_query_by_utc(setup_data_table, populate_data_table):
+    """Test to query by met_in_utc."""
+    data_table = setup_data_table["data_table"]
+    expected_items = populate_data_table
+
+    response = data_table.query(KeyConditionExpression=Key("instrument").eq("mag"))
+
+    items = response["Items"]
+
+    for item in range(len(items)):
+        assert items[item] == expected_items[item]
+
+    response = data_table.query(
+        KeyConditionExpression=Key("instrument").eq("mag")
+        & Key("met_in_utc").between("2021-00-00T00:00:00", "2021-01-02T00:00:00")
+    )
+    items = response["Items"]
+    assert len(items) == 1
+    assert items[0]["met_in_utc"] == expected_items[0]["met_in_utc"]
+
+
+def test_data_query_by_last_modified(setup_data_table, populate_data_table):
+    """Test to query by last_modified."""
+    data_table = setup_data_table["data_table"]
+    expected_items = populate_data_table
+
+    response = data_table.query(
+        IndexName="last_modified",
+        KeyConditionExpression=Key("instrument").eq("mag")
+        & Key("last_modified").begins_with("2021-01"),
+    )
+    items = response["Items"]
+    assert len(items) == 1
+    assert items[0] == expected_items[0]
+
+
 def test_algorithm_query_by_met(setup_dynamodb, populate_algorithm_table):
     """Test to query by met."""
     algorithm_table = setup_dynamodb["algorithm_table"]
