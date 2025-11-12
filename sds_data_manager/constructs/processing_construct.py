1bf4c55579c09af3303b7b6bcb62a83111e0ed0f
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2025-11-12 16:13:00 -0700

    fixed string

--- sds_data_manager/constructs/processing_construct.py
+++ sds_data_manager/constructs/processing_construct.py
@@ -113,7 +113,7 @@ class ProcessingConstruct(Construct):
         # TODO : optimize ULTRA and Lo l1a to use less resources
         cpu = 4
         memory = cdk.Size.gibibytes(16)
-        if "ultra" in job_name or "lo_l1a" in job_name:
+        if "ultra" in job_name or "lo" in job_name:
             cpu = 8
             memory = cdk.Size.gibibytes(48)
         # Create the job definition
