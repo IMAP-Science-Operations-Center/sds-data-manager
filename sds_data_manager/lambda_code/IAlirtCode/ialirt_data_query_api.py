0304197b65431aa7e364f8c1ba164cea3f645fb9
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2026-01-06 11:16:38 -0700

    update restricted fields

--- sds_data_manager/lambda_code/IAlirtCode/ialirt_data_query_api.py
+++ sds_data_manager/lambda_code/IAlirtCode/ialirt_data_query_api.py
@@ -26,7 +26,8 @@ FULL_SCOPES = {
 RESTRICTED_FIELDS = {
     "hit_e_a_side_high_en",
     "hit_e_b_side_high_en",
-    "hit_h_a_side_high_enhit_h_b_side_high_en",
+    "hit_h_a_side_high_en",
+    "hit_h_b_side_high_en",
 }
 
 PUBLIC_CUTOFF_UTC = "2026-02-01T00:00:00"
