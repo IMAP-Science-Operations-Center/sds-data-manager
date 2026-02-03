95375a3df669985be56b8689a1c8454fffc8058f
Laura Sandoval <laurasandoval@Lauras-MacBook-Pro.local>
2026-02-03 16:54:34 -0700

    update cdf inputs

--- sds_data_manager/lambda_code/IAlirtCode/ialirt_archive.py
+++ sds_data_manager/lambda_code/IAlirtCode/ialirt_archive.py
@@ -125,7 +125,9 @@ def lambda_handler(event, context):
     dataset = create_xarray_from_records(all_items)
     dataset.attrs["Data_version"] = "001"
     dataset.attrs["Start_date"] = seven_days_ago.strftime("%Y%m%d")
-    test_data_path = write_cdf(dataset, istp=True)
+    test_data_path = write_cdf(
+        dataset, istp=True, compression=None, auto_fix_depends=False
+    )
 
     output_key = f"archive/{test_data_path.name}"
 
