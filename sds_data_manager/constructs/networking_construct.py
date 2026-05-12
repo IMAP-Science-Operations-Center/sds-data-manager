2dc76a14808a493b6f8f10a114b4119b82d1056e
Laura Sandoval <laura.sandoval@lasp.colorado.edu>
2026-05-12 16:14:13 -0600

    add virtual private gateway that decrypts the incoming IPSec packet and hands it to the vpc

--- sds_data_manager/constructs/networking_construct.py
+++ sds_data_manager/constructs/networking_construct.py
@@ -1,5 +1,6 @@
 """Configure the networking components."""
 
+import aws_cdk as cdk
 from aws_cdk import aws_ec2 as ec2
 from constructs import Construct
 
@@ -52,3 +53,38 @@ class NetworkingConstruct(Construct):
                 ),
             ],
         )
+
+        # Create the Virtual Private Gateway (VGW).
+        # The VGW decrypts incoming IPSec packets from NOAA and hands them into the VPC.
+        self.vpn_gateway = self._create_vpn_gateway()
+
+    def _create_vpn_gateway(self) -> ec2.CfnVpnGateway:
+        """Create a Virtual Private Gateway and attach it to the VPC."""
+        # Create the Virtual Private Gateway (VGW).
+        vpn_gateway = ec2.CfnVpnGateway(
+            self,
+            "VpnGateway",
+            # IPSec version 1 is the standard protocol for encrypted VPN tunnels.
+            type="ipsec.1",
+            tags=[cdk.CfnTag(key="Name", value="ialirt-vpn-gateway")],
+        )
+
+        # Attach the VGW to the VPC so decrypted traffic can enter the VPC.
+        ec2.CfnVPCGatewayAttachment(
+            self,
+            "VpnGatewayAttachment",
+            vpc_id=self.vpc.vpc_id,
+            vpn_gateway_id=vpn_gateway.ref,
+        )
+
+        # Adds VPN route propagation to each public subnet so that regardless of
+        # which AZ the I-ALiRT EC2 lands in, it can receive traffic from the VPN.
+        for i, subnet in enumerate(self.vpc.public_subnets):
+            ec2.CfnVPNGatewayRoutePropagation(
+                self,
+                f"RoutePropagate{i}",
+                route_table_ids=[subnet.route_table.route_table_id],
+                vpn_gateway_id=vpn_gateway.ref,
+            )
+
+        return vpn_gateway
