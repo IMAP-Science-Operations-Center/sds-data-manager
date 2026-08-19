bb19530653fb0b0a24f7e079b99c7e90111ea500
Bryan Harter <41062454+bryan-harter@users.noreply.github.com>
2026-08-19 15:42:54 +0000

    Adding the read-only dashboard to "processing.imap-mission.com"

--- sds_data_manager/constructs/dagster_construct.py
+++ sds_data_manager/constructs/dagster_construct.py
@@ -17,6 +17,7 @@ from aws_cdk import (
 from aws_cdk import (
     aws_ecs_patterns as ecs_patterns,
 )
+from aws_cdk import aws_elasticloadbalancingv2 as elbv2
 from aws_cdk import (
     aws_iam as iam,
 )
@@ -237,6 +238,9 @@ class DagsterEcsConstruct(Construct):
         sg,
         dagster_env_vars,
         db_secret,
+        certificate,
+        root_domain_name,
+        domain,
         **kwargs,
     ) -> None:
         """Initialize the Construct."""
@@ -397,28 +401,13 @@ class DagsterEcsConstruct(Construct):
                 },
             ),
             public_load_balancer=True,
-            open_listener=False,
+            open_listener=True,
             health_check_grace_period=cdk.Duration.seconds(300),
+            certificate=certificate,
+            domain_name=f"processing.{root_domain_name}",
+            domain_zone=domain.hosted_zone,
+            redirect_http=True,
         )
-        allowed_readonly_cidrs = [
-            "128.138.131.0/24",  # LASP
-            "128.112.0.0/16",  # Princeton
-            "140.180.0.0/16",  # Princeton
-            "204.153.48.0/22",  # Princeton
-            "12.161.8.0/24",  # Princeton
-            "12.161.10.0/24",  # Princeton
-            "12.161.14.0/24",  # Princeton
-            "66.180.176.0/24",  # Princeton
-            "66.180.177.0/24",  # Princeton
-            "66.180.184.0/22",  # Princeton
-            "132.177.251.17/32",  # UNH
-        ]
-
-        for cidr in allowed_readonly_cidrs:
-            readonly_webserver_service.load_balancer.connections.allow_from(
-                ec2.Peer.ipv4(cidr),
-                ec2.Port.tcp(80),
-            )
 
         # Allow the new read-only container to talk to the RDS database
         readonly_webserver_service.service.connections.allow_to(
@@ -430,6 +419,26 @@ class DagsterEcsConstruct(Construct):
             timeout=cdk.Duration.seconds(120),
             interval=cdk.Duration.seconds(300),
             unhealthy_threshold_count=3,
+        )
+
+        readonly_webserver_service.listener.add_action(
+            "OidcAuthRule",
+            priority=1,
+            conditions=[elbv2.ListenerCondition.path_patterns(["/*"])],
+            action=elbv2.ListenerAction.authenticate_oidc(
+                authorization_endpoint="https://lasp-auth.colorado.edu/auth/realms/lasp/protocol/openid-connect/auth",
+                token_endpoint="https://lasp-auth.colorado.edu/auth/realms/lasp/protocol/openid-connect/token",  # noqa: S106
+                user_info_endpoint="https://lasp-auth.colorado.edu/auth/realms/lasp/protocol/openid-connect/userinfo",
+                issuer="https://lasp-auth.colorado.edu/auth/realms/lasp",
+                client_id="imap-processing",
+                client_secret=cdk.SecretValue.secrets_manager(
+                    "dagster_oidc_client_secret"
+                ),
+                scope="openid profile",
+                next=elbv2.ListenerAction.forward(
+                    target_groups=[readonly_webserver_service.target_group]
+                ),
+            ),
         )
 
         # Dagster Daemon
